4cb3842a40df5385ce6ccb4de1370197c9075e8c
olivergy <sicake_c@163.com>
2023-04-21 16:14:22 +0800

    Fix: 调整路由. Closes #4

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from backend.views.data_show import *
 app = Flask(__name__)
 
 # 注册蓝图
-app.register_blueprint(ds)
+app.register_blueprint(ds, url_prefix = '/dataShow' )
 
 app.config.from_object(settings)
 db.init_app(app)
@@ -15,6 +15,7 @@ db.init_app(app)
 @app.route('/')
 def hello_world():
     return 'hello world'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
