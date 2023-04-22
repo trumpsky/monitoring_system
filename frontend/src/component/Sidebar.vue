2f0a2544a6a53e2af351f21c1d5509e532e1e8a1
olivergy <sicake_c@163.com>
2023-04-22 23:16:39 +0800

    Feature:搭建商城. Closes #11

--- frontend/src/component/Sidebar.vue
+++ frontend/src/component/Sidebar.vue
@@ -10,7 +10,7 @@
              router>
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-location"></i>
+          <i class="el-icon-info"></i>
           <span>监控系统</span>
         </template>
         <el-menu-item-group>
@@ -28,6 +28,17 @@
           <el-menu-item index="/menu/2-1">集群数据</el-menu-item>
           <el-menu-item index="/menu/2-2">节点单指标</el-menu-item>
           <el-menu-item index="/menu/2-3">节点多指标</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-goods"></i>
+          <span>商店</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/menu/3-1">会员</el-menu-item>
+          <el-menu-item index="/menu/3-2">超级会员</el-menu-item>
+          <el-menu-item index="/menu/3-3">豪华会员</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
---
+++ frontend/src/pages/Product.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <el-empty description="请等待开发结束"></el-empty>
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in 1" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="./../assets/images/skm.jpg" class="image">
+          <div style="padding: 14px;">
+            <span>好吉利的数字</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button" @click="thanks">我已助力！</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+
+</template>
+
+<script>
+import {Message} from 'element-ui'
+
+export default {
+  name: "Product",
+  methods: {
+    thanks() {
+      Message.success("感谢您的助力！系统好感度 +1 ！")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+</style>
