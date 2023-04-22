53236db488fa75df0cc46c737a5cb235ffb3221f
zxnstc <xiaoni2001@163.com>
2023-04-22 10:46:47 +0800

    Add：前端增加集群模式的选择器.Closes#6

---
+++ frontend/src/component/SelectionCluster.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="block">
+  <span class="demonstration">多选选择任意一级选项</span>
+  <el-cascader
+    :options="options"
+    :props="{ multiple: true, checkStrictly: true }"
+    clearable></el-cascader>
+</div>
+
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        options: [{
+          value: 'cc-cc408-hya',
+          label: 'cc-cc408-hya',
+          children: [{
+            value: 'elasticsearch_cluster_health_active_shards',
+            label: 'elasticsearch_cluster_health_active_shards',
+          }, {
+            value: 'elasticsearch_cluster_health_number_of_nodes',
+            label: 'elasticsearch_cluster_health_number_of_nodes',
+          },{
+            value: 'elasticsearch_cluster_health_status',
+            label: 'elasticsearch_cluster_health_status',
+          }]
+        }, {
+          value: 'cc-cc553-interestPrice',
+          label: 'cc-cc553-interestPrice',
+          children: [{
+            value: 'elasticsearch_cluster_health_active_shards',
+            label: 'elasticsearch_cluster_health_active_shards',
+          }, {
+            value: 'elasticsearch_cluster_health_number_of_nodes',
+            label: 'elasticsearch_cluster_health_number_of_nodes',
+          },{
+            value: 'elasticsearch_cluster_health_status',
+            label: 'elasticsearch_cluster_health_status',
+          }]
+        }, ]
+      };
+    }
+  };
+</script>
