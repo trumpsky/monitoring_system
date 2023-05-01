de5699c9f3c3d644b78791dea6d18e25d6ec13ff
zxnstc <xiaoni2001@163.com>
2023-05-01 16:39:45 +0800

    Add:后端增加节点多模式查询接口. Closes #23

--- backend/backend/views/data_show.py
+++ backend/backend/views/data_show.py
@@ -1,6 +1,7 @@
 import json
 from flask import Blueprint, jsonify, request
 from model import User, ClusterData, Cluster, Indicator,Node,NodeSingleData,Disk,NodeMultipleData
+import json
 import helper.data_process as dp
 from sqlalchemy import distinct
 
@@ -29,7 +30,7 @@ def index():
 
 
 @ds.route("/getClusterData", strict_slashes=False, methods=["POST", "GET"])
-def getClusterData():
+def get_cluster_data():
     # start_time = request.form["start_time"]
     # end_time = request.form["end_time"]
     end_time = "2023/04/12 17:35"
@@ -41,63 +42,104 @@ def getClusterData():
     # indicator_ids = request.form["indicator_ids"]
     indicator_ids = [1, 2]
 
-    result = ClusterData.query.filter(ClusterData.time>start_time, 
-                                      ClusterData.time<end_time, 
-                                      ClusterData.cluster_id.in_(cluster_ids), 
-                                      ClusterData.indicator_id.in_(indicator_ids)).order_by(ClusterData.cluster_id,ClusterData.indicator_id,ClusterData.time).all()
+    result = ClusterData.query.filter(ClusterData.time > start_time,
+                                      ClusterData.time < end_time,
+                                      ClusterData.cluster_id.in_(cluster_ids),
+                                      ClusterData.indicator_id.in_(indicator_ids)).order_by(ClusterData.cluster_id, ClusterData.indicator_id, ClusterData.time).all()
     result_list = []
     for item in result:
         result_list.append(item.to_json())
     return jsonify(result_list=result_list)
 
 
-
 @ds.route("/NodeSingleData", strict_slashes=False, methods=["POST", "GET"])
-def getNodeId():
-    cluster_id=request.form["cluster_id"]
-    node_ip=request.form["node_ip"]
-    node_name=request.form["node_name"]
-    result=Node.query.filter(Node.cluster_id==cluster_id,Node.node_ip==node_ip,Node.node_name==node_name).first()
+def get_node_id(node_ip, node_name, cluster_id):
+    result = Node.query.filter(Node.cluster_id == cluster_id,
+                               Node.node_ip == node_ip,
+                               Node.node_name == node_name).first()
     return result.node_id
 
-def getNodeSingleData():
+
+def get_node_single_data():
     # start_time = request.form["start_time"]
     # end_time = request.form["end_time"]
     end_time = "2023/04/12 17:35"
     start_time = "2023/04/01 00:00"
     start_time = dp.datetime_to_timestamp(start_time)
     end_time = dp.datetime_to_timestamp(end_time)
-    cluster_ids=request.form["cluster_ids"]
-    result=NodeSingleData.query.filter(NodeSingleData.time>start_time,
-                                       NodeSingleData.time<end_time,
-                                       NodeSingleData.node_id).order_by(NodeSingleData.indicator_id,NodeSingleData.node_id,NodeSingleData.time).all()
-    
-   
-    result_list=[]
-    node_dict_id=0
-    indicator_id=0
-    node_dict={}
-    point_dict={}
-    for item in result:
+    cluster_ids = request.form["cluster_ids"]
 
-        if (node_dict_id != item.node_id)and(node_dict_id!=0):
-            node_dict[node_dict_id]=point_dict
-            node_dict_id=item.node_id
-            point_dict={}
+    result = NodeSingleData.query.filter(NodeSingleData.time > start_time,
+                                         NodeSingleData.time < end_time,
+                                         NodeSingleData.node_id).order_by(NodeSingleData.indicator_id, NodeSingleData.node_id, NodeSingleData.time).all()
 
-        if (indicator_id != item.indicator_id) and (indicator_id != 0):
+    result_list = []
+    node_dict_id = 0
+    indicator_id = 0
+    node_dict = {}
+    point_dict = {}
+    for i in range(len(result)):
+
+        if (node_dict_id != result[i].node_id) and (node_dict_id != 0):
+            node_dict[node_dict_id] = point_dict
+            node_dict_id = result[i].node_id
+            point_dict = {}
+
+        if (indicator_id != result[i].indicator_id) and (indicator_id != 0):
             result_list.append(node_dict.to_json())
-            indicator_id=item.indicator_id
-            node_dict={}
+            indicator_id = result[i].indicator_id
+            node_dict = {}
 
-        #点的字典
-        point_dict["time"]=item.time
-        point_dict["value"]=item.value
+        # 点的字典
+        point_dict["time"] = result[i].time
+        point_dict["value"] = result[i].value
 
+        if i == len(result)-1:
+            result_list.append(node_dict.to_json())
 
     return jsonify(result_list)
 
 
+@ds.route("/NodeMultipleData", strict_slashes=False, methods=["POST", "GET"])
+def get_disk_id(cluster_id, node_ip, node_name, disk_name):
+    result = Disk.query.filter(Disk.cluster_id == cluster_id,
+                               Disk.node_ip == node_ip,
+                               Disk.node_name == node_name,
+                               Disk.disk_name == disk_name).first()
+    return result.disk_id
+
+
+def get_node_multiple_data():
+
+    result = NodeMultipleData.query.filter(NodeMultipleData.time > start_time,
+                                           NodeMultipleData.time < end_time,
+                                           NodeMultipleData.disk_id).order_by(NodeMultipleData.indicator_id, NodeMultipleData.disk_id, NodeMultipleData.time).all()
+
+    result_list = []
+    disk_dict_id = 0
+    indicator_id = 0
+    disk_dict = {}
+    point_dict = {}
+    for i in range(len(result)):
+
+        if (disk_dict_id != result[i].disk_id) and (disk_dict_id != 0):
+            disk_dict[disk_dict_id] = point_dict
+            disk_dict_id = result[i].disk_id
+            point_dict = {}
+
+        if (indicator_id != result[i].indicator_id) and (indicator_id != 0):
+            result_list.append(disk_dict.to_json())
+            indicator_id = result[i].indicator_id
+            disk_dict = {}
+
+        # 点的字典
+        point_dict["time"] = result[i].time
+        point_dict["value"] = result[i].value
+
+        if i == len(result)-1:
+            result_list.append(disk_dict.to_json())
+
+    return jsonify(result_list)
 
 
 
